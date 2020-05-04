d778cdfba45333ccebf101d8d3d61a4d11a9be65
Romanf5 <roman.f5@gmail.com>
2020-05-04 13:00:36 +0300

    add app selector

---
+++ src/components/TypingSignature/SignatureSettings/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="signature-settings">
+        <div class="signature-settings--type">
+            <app-selector
+                 v-model="type"
+                :list="typeList"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import AppSelector from "@/components/global/AppSelector";
+
+export default {
+    components: { "app-selector": AppSelector },
+    data() {
+        return {
+            typeList: [
+                { text: "Handwriting", value: 1 },
+                { text: "Sans Serif", value: 2 }
+            ],
+            type: false
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+$settings-border: 1px solid $appColor4;
+
+.signature-settings {
+    padding: 30px 0;
+    border: {
+        bottom: $settings-border;
+        top: $settings-border;
+    }
+
+    &--type {
+        max-width: 280px;
+    }
+}
+</style>
--- src/components/TypingSignature/index.vue
+++ src/components/TypingSignature/index.vue
@@ -1,13 +1,33 @@
 <template>
     <div class="typing">
-        <signature-title>Type your signature</signature-title>
+        <signature-title class="typing--title">Type your signature</signature-title>
+        <typing-input class="typing--input"/>
+        <signature-settings class="typing--settings"/>
     </div>
 </template>
 
 <script>
 import SignatureTitle from "@/components/global/SignatureTitle";
+import TypingInput from "./TypingInput";
+import SignatureSettings from "./SignatureSettings";
 
 export default {
-    components: { "signature-title": SignatureTitle }
+    components: {
+        "signature-title": SignatureTitle,
+        "typing-input": TypingInput,
+        "signature-settings": SignatureSettings
+    }
 };
 </script>
+
+<style lang="scss" scoped>
+.typing {
+    &--title {
+        margin-bottom: 30px;
+    }
+
+    &--input {
+        margin-bottom: 30px;
+    }
+}
+</style>
