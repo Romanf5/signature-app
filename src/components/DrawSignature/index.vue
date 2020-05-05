42f1510bda6ab074fdb43311cf92a68ea76af2eb
Romanf5 <roman.f5@gmail.com>
2020-05-05 12:31:14 +0300

    add base component

---
+++ src/components/DrawSignature/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="draw">
+        <signature-title class="typing--title">Draw your eSiganture here</signature-title>
+    </div>
+</template>
+
+<script>
+import SignatureTitle from "@/components/global/SignatureTitle";
+
+export default { components: { "signature-title": SignatureTitle } };
+</script>
