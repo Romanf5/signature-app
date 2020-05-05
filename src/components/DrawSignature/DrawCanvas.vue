1a4f655a5440eeaa85c50d02929d89ab7bd02c9b
Romanf5 <roman.f5@gmail.com>
2020-05-05 12:58:35 +0300

    save commit

---
+++ src/components/DrawSignature/DrawCanvas.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="draw-canvas">
+        <canvas
+            @mousedown="mousePressed = true"
+            @mouseup="mousePressed = false"
+            ref="canvas"
+            class="draw-canvas--canvas"></canvas>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mousePressed: false,
+            color: "#0B2034",
+            width: 3
+        }
+    },
+    computed: {
+        canvas() {
+            return this.$refs.canvas.getContext("2d");
+        }
+    },
+    methods: {
+        draw(x, y) {
+            this.canvas.beginPath();
+            this.canvas.strokeStyle = this.color
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.draw-canvas {
+    &--canvas {
+        border: 1px solid $appColor2;
+        border-radius: 32px;
+        width: 100%;
+        height: 190px;
+    }
+}
+</style>
--- src/components/DrawSignature/index.vue
+++ src/components/DrawSignature/index.vue
@@ -1,11 +1,31 @@
 <template>
     <div class="draw">
-        <signature-title class="typing--title">Draw your eSiganture here</signature-title>
+        <signature-title class="draw--title">Draw your eSiganture here</signature-title>
+        <draw-canvas class="draw--canvas"/>
     </div>
 </template>
 
 <script>
 import SignatureTitle from "@/components/global/SignatureTitle";
+import DrawCanvas from "./DrawCanvas";
 
-export default { components: { "signature-title": SignatureTitle } };
+export default {
+    components: {
+        "signature-title": SignatureTitle,
+        "draw-canvas": DrawCanvas
+    }
+};
 </script>
+
+<style lang="scss" scoped>
+.draw {
+    &--title {
+        margin-bottom: 30px;
+    }
+
+    &--canvas {
+        max-width: 865px;
+        margin: 0 auto;
+    }
+}
+</style>
